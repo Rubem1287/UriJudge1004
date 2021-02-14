e0b3033cb761b051e95afb364fad87853518de71
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-14 18:21:39 -0300

    Add files via upload

---
+++ Uri 1004/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uri_1004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, prod;
+
+            Console.WriteLine();
+            Console.Write("Digite o primeiro número : ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Digite o Segundo número : ");
+            b = int.Parse(Console.ReadLine());
+
+            prod = a * b;
+
+            Console.WriteLine();
+            Console.Write("PROD = "+prod);
+
+
+
+        }
+
+
+
+    }
+}
